90e27d42127bf7f01c224f1c3272ac36227b99b2
kungjim <7629149+kungjim@users.noreply.github.com>
2023-09-17 23:07:38 +0800

    update pypi action

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ class CMakeBuild(build_ext):
 
         # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
         # from Python.
-        venv_dir = os.path.abspath(sys.executable+"../../")
+        venv_dir = os.path.abspath(sys.executable+"/../../")
         walk_dir(venv_dir)
         cmake_args = [
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
